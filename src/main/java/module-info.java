da3e4439301be0f1c338f2170f731d212adbcefa
Simon <sl2057@cam.ac.uk>
2022-05-12 14:47:17 +0100

    Add javafx.web to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module uk.ac.cam.group09.mycricket {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires javafx.web;
 
     opens uk.ac.cam.group09.mycricket to javafx.fxml;
     exports uk.ac.cam.group09.mycricket;
