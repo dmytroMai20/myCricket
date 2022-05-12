b5bdd5d96068e739d0de4e2956807826c659975c
Simon <sl2057@cam.ac.uk>
2022-05-12 14:19:14 +0100

    Commit

--- src/main/java/uk/ac/cam/group09/mycricket/HelloApplication.java
+++ src/main/java/uk/ac/cam/group09/mycricket/HelloApplication.java
@@ -11,8 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        // The aspect ratio of iPhone 13 screen is 9:19.5
+        Scene scene = new Scene(fxmlLoader.load(), 90*3, 195*3);
+        stage.setTitle("MyCricket");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/uk/ac/cam/group09/mycricket/WeatherConditions.java
+++ src/main/java/uk/ac/cam/group09/mycricket/WeatherConditions.java
@@ -15,7 +15,30 @@ public class WeatherConditions {
         datetime = dateTime;
     }
     public boolean goodToPlay(){
-        //TODO logic to decide if the conditions are playable
+        /*
+        Heat illness risk
+        | Temp  | humidity |   risk   | message
+        |-------|----------|----------|------------------------
+        | 24-28 |  > 70%   |   low    | exercise caution
+        | 28-31 |  > 60%   | moderate | increase vigilance and monitor players. consider increasing drink breaks
+        | 31-35 |  > 50%   |   high   | uncomfortable for most. increase drink breaks, batters esp. be wary
+        | 35-37 |  > 30%   | v. high  | consider reducing overs or delaying game
+        |  37+  |  > 30%   |  extreme | players should leave the field
+
+        UV
+        | uvindex | severrisk | message
+
+        Rainfall
+        | precip | message
+
+        Visibility
+        | visibility | message
+
+        Wind
+        | windgust | windspeed | message
+
+        Conditions: (if snowy etc. then...)
+         */
         return true;
     }
     public void setData(HashMap<String,Object> data){
